508641266f69e535d0bf5b21e7770d029b542c7f
Christoph Jerolimov <jerolimov+git+2022@gmail.com>
2022-01-23 23:42:32 +0100

    Change to GRAPHCMS_URL

--- migration/graphql.js
+++ migration/graphql.js
@@ -1,14 +1,15 @@
 const { GraphQLClient } = require('graphql-request');
 
-const baseURL = process.env.GRAPHCMS_URL;
+if (!process.env.GRAPHCMS_URL) {
+  throw new Error('Environment variable GRAPHCMS_URL not defined!');
+}
+if (!process.env.GRAPHCMS_API_TOKEN) {
+  throw new Error('Environment variable GRAPHCMS_API_TOKEN not defined!');
+}
 
-const region = process.env.GRAPHCMS_REGION;
-const projectId = process.env.GRAPHCMS_PROJECT_ID;
-const environment = process.env.GRAPHCMS_ENVIRONMENT;
+const url = process.env.GRAPHCMS_URL;
 const apiToken = process.env.GRAPHCMS_API_TOKEN;
-// const stage = process.env.GRAPHCMS_DEFAULT_STAGE || 'PUBLISHED';
 
-const url = baseURL || `https://api-${region}.graphcms.com/v2/${projectId}/${environment}`;
 const options = {
   headers: {
     ...apiToken ? { 'Authorization': `Bearer ${apiToken}` } : null,
